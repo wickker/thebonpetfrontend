01c6b76880f46bb97e6bd29618c9f328d8e3cbba
Dian Chun Choy <choydianchun@gmail.com>
2025-05-17 14:57:39 +0800

    [master]: tidy up login component

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const LOCAL_STORAGE_KEYS = {
+  CODE_VERIFIER: 'tbp_code_verifier',
+  NONCE: 'tbp_nonce',
+  CUSTOMER_ACCESS_TOKEN: 'tbp_customer_access_token',
+} as const
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_ENVIRONMENT: string
+  readonly VITE_FE_BASE_URL: string
+  readonly VITE_SHOPIFY_CUSTOMER_ACCOUNT_API_CLIENT_ID: string
+  readonly VITE_SHOPIFY_CUSTOMER_AUTHENTICATION_BASE_URL: string
 }
 
 interface ImportMeta {
