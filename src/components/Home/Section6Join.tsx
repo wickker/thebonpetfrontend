1663e31fc5ca5d3840fabff8a3b7539b59d9de74
Dian Chun Choy <choydianchun@gmail.com>
2025-06-06 18:04:48 +0800

    [personal/dian]: add ig button

--- src/components/Home/Section6Join.tsx
+++ src/components/Home/Section6Join.tsx
@@ -1,5 +1,5 @@
 import { InstagramEmbed } from 'react-social-media-embed'
-import { FaWhatsapp } from 'react-icons/fa6'
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa6'
 import { FiExternalLink } from 'react-icons/fi'
 
 const Section6Join = () => {
@@ -12,16 +12,29 @@ const Section6Join = () => {
           Meet our satisfied paw-rents
         </p>
 
-        <div className='flex flex-wrap items-center justify-around gap-4'>
+        <div className='mb-6 flex flex-wrap items-center justify-around gap-4'>
           <InstagramEmbed url='https://www.instagram.com/reel/DFZVNdKzQa6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' />
           <InstagramEmbed url='https://www.instagram.com/reel/DFZVNdKzQa6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' />
           <InstagramEmbed url='https://www.instagram.com/reel/DFZVNdKzQa6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' />
           <InstagramEmbed url='https://www.instagram.com/reel/DFZVNdKzQa6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' />
         </div>
+
+        <a
+          className='flex cursor-pointer items-center gap-x-2 rounded-xl border-[2px] border-white/70 bg-[radial-gradient(100%_100%_at_0%_100%,#F8BA71_22.92%,#F27979_46.35%,#EC69DB_79.35%,#BE5BED_100%)] px-3 py-2 text-white transition-all hover:scale-x-102 hover:shadow-[0_0_10px_rgba(0,0,0,0.3)]'
+          href='https://www.instagram.com/thebonpet'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaInstagram className='h-10 w-10' />
+          <div className='flex flex-col items-center'>
+            <p className='font-bold'>@TheBonPet</p>
+            <p className='text-xs'>Follow on Instagram</p>
+          </div>
+        </a>
       </div>
 
       <a
-        className='group absolute top-8 right-0 flex cursor-pointer items-center gap-x-2 rounded-l-full border-[2px] border-r-0 border-white/50 px-4 py-2 text-white'
+        className='group absolute top-8 right-0 hidden cursor-pointer items-center gap-x-2 rounded-l-full border-[2px] border-r-0 border-white/50 px-4 py-2 text-white lg:flex'
         style={{
           background: 'linear-gradient(90deg, #4EC465 22.95%, #3CA250 100%)',
         }}
