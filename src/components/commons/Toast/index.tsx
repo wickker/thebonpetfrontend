073ce4a413fe20ee4b43d6e5e4d8e28ee01b97ce
Dian Chun Choy <choydianchun@gmail.com>
2025-06-06 15:58:46 +0800

    [personal/dian]: fix toast

--- src/components/commons/Toast/index.tsx
+++ src/components/commons/Toast/index.tsx
@@ -7,12 +7,14 @@ const Toast = () => {
   const { toast, toasts } = useToastContext()
 
   return createPortal(
-    <div className='z-toast fixed top-0 right-0 left-0 mx-auto flex max-w-lg flex-col items-center gap-y-3 py-4'>
-      <AnimatePresence>
-        {toasts.map((t) => (
-          <Tile key={t.id} onClose={() => toast.close(t.id)} {...t} />
-        ))}
-      </AnimatePresence>
+    <div className='z-toast fixed top-0 right-0 left-0 flex w-full justify-center'>
+      <div className='mt-4 flex w-full max-w-lg flex-col items-center gap-y-3 px-4'>
+        <AnimatePresence>
+          {toasts.map((t) => (
+            <Tile key={t.id} onClose={() => toast.close(t.id)} {...t} />
+          ))}
+        </AnimatePresence>
+      </div>
     </div>,
     document.body
   )
