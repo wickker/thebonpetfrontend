c956ec0e1167524696b27d6671a9d24b7d30c2f5
Dian Chun Choy <choydianchun@gmail.com>
2025-06-09 17:31:44 +0800

    [personal/dian]: add featured article section

--- src/components/Home/Section7Faqs/index.tsx
+++ src/components/Home/Section7Faqs/index.tsx
@@ -1,8 +1,15 @@
+import { MdOutlineArticle } from 'react-icons/md'
+import useBlog from '@/hooks/queries/useBlog'
 import { ROUTES } from '@/utils/constants'
 import Accordian from './Accordian'
+import ArticleSkeleton from './ArticleSkeleton'
 import { FAQS } from './data'
 
 const Section7Faqs = () => {
+  const { useGetArticlesQuery } = useBlog()
+  const getArticles = useGetArticlesQuery()
+  const article = getArticles.data?.slice().reverse()?.[0]?.node
+
   return (
     <div
       style={{
@@ -10,12 +17,12 @@ const Section7Faqs = () => {
       }}
     >
       <div className='mx-auto flex w-full max-w-[1650px] flex-col items-center px-4 py-8 lg:w-[90%]'>
-        <div className='grid w-full gap-6 lg:grid-cols-[3fr_auto_1.6fr]'>
-          <div className='flex flex-col'>
-            <h1 className='text-dark-brown mt-4 mb-8 text-left text-5xl font-bold'>
-              Frequently Asked Questions
-            </h1>
+        <div className='grid w-full items-start gap-10 lg:grid-cols-[3fr_auto_1.6fr]'>
+          <h1 className='text-dark-brown col-span-full mt-4 text-left text-5xl font-bold'>
+            Frequently Asked Questions
+          </h1>
 
+          <div className='flex flex-col'>
             <div className='flex flex-col gap-y-6'>
               {FAQS.slice(0, 5).map((faq) => (
                 <Accordian
@@ -37,7 +44,40 @@ const Section7Faqs = () => {
             </p>
           </div>
 
-          <div className='hidden w-[2px] rounded-full bg-[#E9D9BD] lg:block' />
+          <div className='hidden h-full w-[2px] rounded-full bg-[#E9D9BD] lg:block' />
+
+          <div className='flex flex-col'>
+            {getArticles.isLoading ? (
+              <ArticleSkeleton />
+            ) : (
+              <div className='bg-beige relative flex h-fit flex-col gap-4 rounded-xl border border-[#CCBC9E] p-4'>
+                <img
+                  src={article?.image?.url || ''}
+                  className='object-start max-h-[220px] rounded-xl object-cover'
+                />
+
+                <h1 className='text-dark-brown text-xl font-bold'>
+                  {article?.title}
+                </h1>
+
+                <p className='text-brown line-clamp-3'>{article?.excerpt}</p>
+
+                <div className='absolute top-[-24px] left-1/2 flex w-fit -translate-x-1/2 transform items-center gap-x-2 rounded-xl border border-[#7B6D57] bg-[#443928] px-4 py-2 text-lg font-bold whitespace-nowrap text-white'>
+                  <MdOutlineArticle />
+                  Featured Article
+                </div>
+              </div>
+            )}
+
+            <a
+              className='text-dark-brown mt-8 text-center text-lg font-bold underline'
+              href={ROUTES.BLOG}
+            >
+              View More
+            </a>
+          </div>
+
+          <div className='col-span-full w-full rounded-xl bg-pink-200 p-6'></div>
         </div>
       </div>
     </div>
